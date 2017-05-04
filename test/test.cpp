263721dfb0fca56feda9ce1170eac4ecd08ec089
Daniele Ferone <danieleferone@gmail.com>
2017-05-04 16:07:15 +0200

    Prova

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 
 #include "sqr.h"
 #include "plus.h"
+#include "blabla.h"
 
 BOOST_AUTO_TEST_CASE(NowPassTest)
 {
@@ -17,4 +18,12 @@ BOOST_AUTO_TEST_CASE(PassTest)
 BOOST_AUTO_TEST_CASE(PlusTest)
 {
 	BOOST_CHECK_EQUAL(5, plus(2,3));
+}
+
+BOOST_AUTO_TEST_CASE(BlablaTest) {
+	blabla b(15);
+
+
+	BOOST_CHECK_EQUAL(15, b.getX());
+
 }
