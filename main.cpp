e9e474119622b4c0a979e6b869b88359031be3c0
Daniele Ferone <daniele.ferone@unina.it>
2022-04-03 10:28:44 +0200

    Swapped to Catch2

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by daniele on 03/04/22.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!\n";
+    return 0;
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,11 @@
 #define BOOST_TEST_MODULE FiniteSetTest
-#include <boost/test/unit_test.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include "FiniteSet.hpp"
 
-BOOST_AUTO_TEST_CASE(NowPassTest)
-{
-  dferone::FiniteSet fs(10);
-  BOOST_CHECK_EQUAL(fs.contains(3), false);
+TEST_CASE( "FiniteSet", "[FiniteSet]" ) {
+    dferone::FiniteSet fs(10);
+    REQUIRE_FALSE(fs.contains(3));
+    fs.add(4);
+    REQUIRE(fs.contains(4));
 }
